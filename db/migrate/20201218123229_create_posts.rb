3b72a01ac3c877d5deb3282a5d62c9fb36d10599
Kulebyakin Andrey <kulebyakin@pop-os.localdomain>
2020-12-20 16:12:14 +0800

    create db, migrate, add pg to gemfile

--- db/migrate/20201218123229_create_posts.rb
+++ db/migrate/20201218123229_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.integer :id
       t.string :title
       t.string :body
       t.integer :created_by
