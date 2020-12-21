51b84124b9cf2f5465d0ba65f04c90422cb732f5
Kulebyakin <kulebyakin@pop-os.localdomain>
2020-12-21 11:20:30 +0800

    Add posts topic routes and constraints for posts id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/new', to: 'posts#new'
+  
+  get 'posts/:id', to: 'posts#show', constraints: { id: /\d+/ }
+  get 'posts/:topic', to: 'posts#topic'
+
   post 'posts/create', to: 'posts#create'
-  get 'posts/:id', to: 'posts#show'
   get 'posts/edit/:id', to: 'posts#edit'
   post 'posts/update/:id', to: 'posts#update'
 
