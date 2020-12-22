bc6ae4ca6fa8082046fd6670557a7067387c704f
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-23 00:49:51 +0800

    Add autoload modules from lib directory

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module BlogRailsReact
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ru
+    config.autoload_paths << "#{Rails.root}/lib"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
