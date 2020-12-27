ab5e62ddc98a98af99efa156f1b32bcd5d48c132
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-27 20:50:33 +0800

    Create post_test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post is valid when all props are specified" do
+    refute build(:post, :about_design).valid?
+  end
+
+  test "post without topic failed validation" do
+    refute build(:post).valid?
+  end
+
+  test "post with empty title fails validation" do
+    refute build(:post, :about_design, title: "").valid?
+  end
+
+  test "post with empty body fails validation" do
+    refute build(:post, :about_design, body: "").valid?
+  end
+
+  test "post with too long title fails validation" do
+    refute build(:post, :about_design, title: "x" * 301).valid?
+  end
 end
