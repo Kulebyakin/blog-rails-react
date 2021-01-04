a02748ef9b5d5b695494c9739e4aefc8a2e90742
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-04 21:05:02 +0800

    Install Action Text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,6 @@ ActiveStorage.start()
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+
+require("trix")
+require("@rails/actiontext")
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :topic
   belongs_to :user
+  has_rich_text :body
 
   validates :title, presence: true, length: { maximum: 300 }
   validates :body, presence: true
